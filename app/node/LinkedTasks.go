c1907cdc91fcd881470134b840441b8c400cbb1e
Vladimir Lila <v.b.lila@yandex.ru>
2023-12-07 18:25:27 +0100

    LinkedTasks bug method Pull()

--- app/node/LinkedTasks.go
+++ app/node/LinkedTasks.go
@@ -16,7 +16,7 @@ func (l *LinkedTasks) Append(task *Task) {
 	node := &TaskNode{prev: nil, next: nil, Task: task}
 	if l.tail == nil {
 		l.tail = node
-		l.head = l.tail
+		l.head = node
 	} else {
 		l.tail.next = node
 		node.prev = l.tail
@@ -37,13 +37,23 @@ func (l *LinkedTasks) Next(curr *TaskNode) *TaskNode {
 func (l *LinkedTasks) Pull(taskNode *TaskNode) {
 	if taskNode.Task.Id == l.head.Task.Id {
 		l.head = taskNode.next
+		if l.head != nil {
+			l.head.prev = nil
+		}
 	}
 	if taskNode.Task.Id == l.tail.Task.Id {
 		l.tail = taskNode.prev
+		if l.tail != nil {
+			l.tail.next = nil
+		}
 	}
 
 	if taskNode.prev != nil {
 		taskNode.prev.next = taskNode.next
+		taskNode.next.prev = taskNode.prev
+
+		taskNode.prev = nil
+		taskNode.next = nil
 	}
 
 	l.length--
